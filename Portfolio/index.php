8e6b29fb7db0835b8b46a1e0e4c1bb6719a8c339
Mariana Valencia <purple.mv@gmail.com>
2018-10-20 21:02:20 -0500

    OOP

--- Portfolio/index.php
+++ Portfolio/index.php
@@ -1,78 +1,9 @@
 <?php
+
+include('jobs.php');
+
 $name = 'Mariana Valencia';
 $limitMonths = 2000;
-$jobs = [
-    [
-        'title' => 'PHP Developer',
-        'description' => 'This is an amazing job, just try it out!',
-        'visible' => true,
-        'months' => 16
-    ],
-    [
-        'title' => 'Python Dev',
-        'visible' => false,
-        'months' => 14
-
-    ],
-    [
-        'title' => 'Devops',
-        'visible' => true,
-        'months' => 5
-
-    ],
-    [
-        'title' => 'Node Dev',
-        'visible' => true,
-        'months' => 24
-
-    ],
-    [
-        'title' => 'Frontend Dev',
-        'visible' => true,
-        'months' => 3
-    ],
-];
-
-//function getDuration($months) {
-//    $years = floor($months / 12);
-//    $extraMonths = $months % 12;
-//    return "$years years $extraMonths months";
-//}
-
-function getDuration($months) {
-    $years = floor($months / 12);
-    $extraMonths = $months % 12;
-    if($months > 12) {
-        if (($months % 12) > 0) {
-            return "$years years $extraMonths months";
-        }
-        else if (($months % 12) == 0) {
-            return "$years years";
-        }
-    }
-    else {
-        return "$months months";
-    }
-
-}
-
-function printJob($job) {
-    if($job['visible'] == false) {
-        return;
-    }
-    echo '<li class="work-position">';
-    echo '<h5>' . $job['title'] . '</h5>';
-    echo '<p>' . $job['description'] . '</p>';
-    echo '<p>' . getDuration($job['months']) . '</p>';
-    echo '<strong>Achievements:</strong>';
-    echo '<ul>';
-    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-    echo '</ul>';
-    echo '</li>';
-};
-
 
 ?>
 
@@ -126,7 +57,7 @@ function printJob($job) {
               <?php
               $totalMonths = 0;
               for($idx = 0; $idx < count($jobs); $idx++) {
-                  $totalMonths += $jobs[$idx]['months'];
+                  $totalMonths += $jobs[$idx]->months;
 
                   if($totalMonths > $limitMonths) {
                       break;
