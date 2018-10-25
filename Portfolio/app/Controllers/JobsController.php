a7abcd7e62d6dc888c4aeb053b82b630e790c6cb
Mariana Valencia <purple.mv@gmail.com>
2018-10-24 21:38:39 -0500

    Adding a validator with error message

--- Portfolio/app/Controllers/JobsController.php
+++ Portfolio/app/Controllers/JobsController.php
@@ -3,24 +3,39 @@
 namespace App\Controllers;
 
 use App\Models\{Job, Project};
+use Respect\Validation\Validator as v;
 
 class JobsController extends BaseController {
     public function getAddJobAction($request) {
+        $responseMessage = null;
+
         $postData = $request->getParsedBody();
         if ($request->getMethod() == 'POST') {
-            if ($postData['job'] == 'job') {
-                $job = new Job();
-                $job->title = $postData['title'];
-                $job->description = $postData['description'];
-                $job->save();
-            } else if($postData['job'] == 'project') {
-                $project = new Project();
-                $project->title = $postData['title'];
-                $project->description = $postData['description'];
-                $project->save();
+            $jobValidator = v::key('title', v::stringType()->notEmpty())
+                ->key('description', v::stringType()->notEmpty());
+
+            try {
+                $jobValidator->assert($postData);
+                if ($postData['job'] == 'job') {
+                    $job = new Job();
+                    $job->title = $postData['title'];
+                    $job->description = $postData['description'];
+                    $job->save();
+                } else if($postData['job'] == 'project') {
+                    $project = new Project();
+                    $project->title = $postData['title'];
+                    $project->description = $postData['description'];
+                    $project->save();
+                }
+                $responseMessage = 'Saved';
+            } catch(\Exception $e) {
+                $responseMessage = $e->getMessage();
             }
+
         }
 
-        echo $this->renderHTML('addJob.twig');
+        return $this->renderHTML('addJob.twig', [
+            'responseMessage' => $responseMessage,
+        ]);
     }
 }
